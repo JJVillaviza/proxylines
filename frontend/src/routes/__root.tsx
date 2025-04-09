a5dcd0ba079d0defcae6b1adc06a9c1382a052fb
JJ Villaviza <joanjoevillaviza15@gmail.com>
2025-04-09 16:04:37 +0800

    frontend layout and error handling setup

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Header } from "@/components/siteHeader";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,7 +11,15 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <Outlet />
+      <div className="flex min-h-screen flex-col text-foreground">
+        <Header />
+        <main className="container mx-auto grow p-4">
+          <Outlet />
+        </main>
+        <footer className="p-4 text-center">
+          <p className="text-sm text-muted-foreground">ProxyLines &copy;</p>
+        </footer>
+      </div>
       <ReactQueryDevtools />
       <TanStackRouterDevtools position="bottom-left" />
     </>
